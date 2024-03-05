0b945b906fad2dbfdbd74f13ffa2dadbf5c14273
Victor de Oliveira <128698672+victor352521@users.noreply.github.com>
2024-03-05 08:36:24 -0300

    Add files via upload

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,22 +1,27 @@
-palavra_secreta = "malakoi";
-letras_acertadas = ["_","_","_","_","_","_","_"]
-tentativas = 10
+def jogar():
 
-while tentativas > 0 and "_" in letras_acertadas:
-    palpite = input("Digite uma letra:").lower()
-    
-    if palpite in palavra_secreta:
-        index = 0
-        for letra in palavra_secreta:
-            if palpite == letra:
-                letras_acertadas[index] = letra
-            index += 1
-    else:
-        tentativas -= 1 
-        print (f"Você tem {tentativas} tentativas restantes");
-    print(" ".join(letras_acertadas))
+    palavra_secreta = "malakoi"
+    letras_acertadas = ["_","_","_","_","_","_","_"]
+    tentativas = 10
+
+    while tentativas > 0 and "_" in letras_acertadas:
+        palpite = input("Digite uma letra:").lower()
         
-if "_" not in letras_acertadas:
-   print (f"Parabéns! Você acertou.")
-else:
-    print (f"Poxa, não foi dessa vez :(")
+        if palpite in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if palpite == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            tentativas -= 1 
+            print (f"Você tem {tentativas} tentativas restantes");
+        print(" ".join(letras_acertadas))
+            
+    if "_" not in letras_acertadas:
+    print (f"Parabéns! Você acertou.")
+    else:
+        print (f"Poxa, não foi dessa vez :(")
+
+if(__name__=="__main__")
+    jogar()
---
+++ jogos.py
@@ -0,0 +1,20 @@
+import jogo_da_forca
+import adivinhacao
+
+def escolha_jogo():
+    print("******************")
+    print("**Escolha o jogo**")
+    print("******************")
+    print("(1)Forca,(2)Advinhação")
+
+    jogo = int(input("Qual jogo você quer jogar hoje?"))
+
+    if (jogo == 1):
+        print("Jogo da forca")
+        jogo_da_forca.jogar()
+    elif (jogo == 2):
+        print("Jogo da advinhação")
+        adivinhacao.jogar_advinhacao()
+
+if(__name__=="__main__")
+    escolha_jogo()
